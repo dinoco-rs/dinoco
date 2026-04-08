5178277cc78f1a95ccae2689b7a6c52faef2a236
Matheus Castro <matheuszastrodev@hotmail.com>
2026-04-08 20:17:20 -0300

    changed tuono.config

--- dinoco_docs/tuono.config.ts
+++ dinoco_docs/tuono.config.ts
@@ -9,7 +9,7 @@ import dinocoGrammar from '../dinoco_vscode/configs/dinoco.tmLanguage.json';
 
 const config: TuonoConfig = {
 	server: {
-		host: '127.0.0.1',
+		host: '0.0.0.0',
 		port: 3000,
 		origin: 'https://docs.dinoco.io',
 	},
